f87a1eccc75c101dda08aada5f4137cb12a734c6
phuoocit <phuoocpro00@gmail.com>
2022-03-04 22:14:27 +0700

    test1

---
+++ ASM/asm2/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Content from './Content';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className='grid'>
+    <Header />
+    <Content />
+    </div>
+  );
+}
+
+export default App;
---
+++ ASM/asm2/src/Header.js
@@ -0,0 +1,12 @@
+
+
+function Header() {
+    
+    return (
+        <header>
+            <h1>Ứng dụng quản lý nhân sự 1.0</h1>
+        </header>
+    )
+}
+
+export default Header;
