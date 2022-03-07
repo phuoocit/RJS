bd8e75ae3ac0eee38dd367db3c434e75888cf575
phuoocit <phuoocpro00@gmail.com>
2022-03-07 16:19:28 +0700

    test

--- ASM/asm1/src/Header.js
+++ ASM/asm1/src/Header.js
@@ -4,7 +4,7 @@ function Header() {
     
     return (
         <header>
-            <h1>Ứng dụng quản lý nhân sự 1.0</h1>
+            <h1>Ứng dụng quản lý nhân sự 1.00</h1>
         </header>
     )
 }
