2e51dc93c65b5c73403dc342b2daac6850f578b2
phuoocit <phuoocpro00@gmail.com>
2022-09-16 06:00:24 +0700

    fix bug

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import HomePage from "./pages/Home";
 import DishPage from "./pages/Dish";
 import AboutPage from "./pages/About";
 import SearchPage from "./pages/Search";
-import SalarySheetpage from "./pages/SalarySheet";
 
 function App() {
   return (
@@ -21,10 +20,6 @@ function App() {
         <Route path="/About" element={<AboutPage></AboutPage>}></Route>
         <Route path="/Contact" element={<ContactPage></ContactPage>}></Route>
         <Route path="/Dish/:id" element={<DishPage></DishPage>}></Route>
-        <Route
-          path="/SalarySheet"
-          element={<SalarySheetpage></SalarySheetpage>}
-        ></Route>
         <Route path="/Search/:id" element={<SearchPage></SearchPage>}></Route>
       </Routes>
       <Footer />
